5096233835529598322d8c915723f18dce3dfcef
Administrator <Administrator@DESKTOP-ISVFPRU>
2020-11-11 11:26:57 +0900

    시험치고있는중

---
+++ ApartmentPrice/src/main/java/com/koreait/apart/HomeController.java
@@ -0,0 +1,21 @@
+package com.koreait.apart;
+
+import java.util.Locale;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+	
+
+	@RequestMapping
+	public String home(Locale locale, Model model) {
+		model.addAttribute("locationList", null);
+		return "index";
+	}
+	
+}
