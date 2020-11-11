710a49e1aa6e1a554c1c8ae73312f547142b9c7e
Administrator <Administrator@DESKTOP-ISVFPRU>
2020-11-11 11:34:43 +0900

    2020.11.11

--- ApartmentPrice/src/main/java/com/koreait/apart/HomeController.java
+++ ApartmentPrice/src/main/java/com/koreait/apart/HomeController.java
@@ -2,19 +2,20 @@ package com.koreait.apart;
 
 import java.util.Locale;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
 	
+	@Autowired
+	HomeMapper mapper;
 
 	@RequestMapping
 	public String home(Locale locale, Model model) {
-		model.addAttribute("locationList", null);
+		model.addAttribute("locationList", mapper.selLocationCodeList());
 		return "index";
 	}
 	
--- ApartmentPrice/src/main/java/com/koreait/apart/HomeMapper.java
+++ ApartmentPrice/src/main/java/com/koreait/apart/HomeMapper.java
@@ -1,8 +1,10 @@
 package com.koreait.apart;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface HomeMapper {
-
+	List<LocationCodeVO> selLocationCodeList();
 }
