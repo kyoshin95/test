58ef07f55ac3ae978abf3362ff571c92757dd922
Administrator <Administrator@DESKTOP-ISVFPRU>
2020-11-11 11:38:27 +0900

    2020.11.11

--- ApartmentPrice/src/main/java/com/koreait/apart/HomeController.java
+++ ApartmentPrice/src/main/java/com/koreait/apart/HomeController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 	
 	@Autowired
-	HomeMapper mapper;
+	private HomeMapper mapper;
 
 	@RequestMapping
 	public String home(Locale locale, Model model) {
